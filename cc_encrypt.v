7f1e36e175b34553084b7a4e8db184b733dc619d
SeongJun-Ro <seongjunro@gmail.com>
2024-08-10 11:56:47 +0900

    add read pt sig
    add i_sig_pt
    add w_mask
    change o_read_pt

--- cc_encrypt.v
+++ cc_encrypt.v
@@ -1,15 +1,16 @@
 module cc_encrypt (
 	input					i_clk, i_rstn,
 	input					i_start,
-
+	
+	input					i_sig_pt,
 	input		[255:0]		i_key,
 	input		[95:0]		i_non,
 	input		[511:0]		i_pt,
 	input		[31:0]		i_len_pt,
 
 	output	reg	[511:0]		o_ct,
-	output	wire			o_done,
-	output	wire			o_read_pt
+	output	reg				o_read_pt,
+	output	wire			o_done
 );
 
 	// ***** local parameter definition *****
@@ -21,6 +22,8 @@ module cc_encrypt (
 	parameter BLOCK	= 2'd1;
 	parameter CRYPT	= 2'd2;
 	parameter DONE	= 2'd3;
+	
+	parameter MASK	= 512'hffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
 	// ***** local register definition *****
 	reg		[1:0]	r_fsm;
@@ -35,11 +38,12 @@ module cc_encrypt (
 	wire			w_blk_start;	// 1: start BLOCK state
 	wire			w_blk_done;		// 1: finish BLOCK state
 	wire	[511:0]	w_stream;		// output 'cc_block'
+	wire	[511:0]	w_mask;
 	
 	assign	w_blk_state	= r_len_pt!=32'd0;
-	assign	w_blk_start	= (i_start) || ((r_fsm == CRYPT) && w_blk_state);
+	assign	w_blk_start	= (i_start) || ((r_fsm == CRYPT) && i_sig_pt);
+	assign	w_mask		= (r_len_pt<32'd64) ? (MASK >> {32'd64-r_len_pt, 3'd0}) : MASK;
 	
-	assign	o_read_pt	= (w_blk_done && w_blk_state);
 	assign	o_done		= (r_fsm == DONE);
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,9 +71,10 @@ module cc_encrypt (
 			r_fsm	<= IDLE;
 		else
 			case (r_fsm)
-				IDLE	: r_fsm <= (i_start) ? BLOCK : IDLE;
-				BLOCK	: r_fsm <= (w_blk_done) ? CRYPT : BLOCK;
-				CRYPT	: r_fsm <= (w_blk_start) ? BLOCK : DONE;
+				IDLE	: r_fsm <=	(i_start)		? BLOCK : IDLE;
+				BLOCK	: r_fsm <=	(w_blk_done)	? CRYPT : BLOCK;
+				CRYPT	: r_fsm <=	(!w_blk_state)	? DONE :
+									(w_blk_start)	? BLOCK : CRYPT;
 				DONE	: r_fsm <= IDLE;
 			endcase
 	end
@@ -100,7 +105,7 @@ module cc_encrypt (
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_pt	<= 512'd0;
-		else if (i_start || o_read_pt)
+		else if (i_start || i_sig_pt)
 			r_pt	<= i_pt;
 //		else
 //			r_pt	<= r_pt;
@@ -111,18 +116,28 @@ module cc_encrypt (
 			r_len_pt	<= 32'd0;
 		else if (i_start)
 			r_len_pt	<= i_len_pt;
-		else if (o_read_pt)
+		else if (w_blk_done)
 			r_len_pt	<= (r_len_pt<32'd64) ? 32'd0 : r_len_pt - 32'd64;
 //		else
 //			r_len_pt	<= r_len_pt;
 	end
 
+	// read o_ct and input pt
+	always @(posedge i_clk, negedge i_rstn) begin
+		if (!i_rstn)
+			o_read_pt	<= 1'd0;
+		else if (w_blk_done && w_blk_state)
+			o_read_pt	<= (r_len_pt<32'd64) ? 1'b0 : 1'b1;
+		else
+			o_read_pt	<= 1'd0;
+	end
+	
 	// encrypt plain text
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			o_ct	<= 512'd0;
 		else
-			o_ct	<= (w_blk_done) ? w_stream ^ r_pt : o_ct;
+			o_ct	<= (w_blk_done) ? w_mask & (w_stream ^ r_pt) : o_ct;
 	end
 
 endmodule 
