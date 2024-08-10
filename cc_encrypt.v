5abc7e48e166f0e4e5c087e11972fcd7f60b2289
SeongJun-Ro <seongjunro@gmail.com>
2024-08-10 11:47:41 +0900

    rm cc_block 1
    change r_cnt timing
    change r_fsm bit 5->4

--- cc_encrypt.v
+++ cc_encrypt.v
@@ -7,82 +7,73 @@ module cc_encrypt (
 	input		[511:0]		i_pt,
 	input		[31:0]		i_len_pt,
 
-	output	reg	[511:0]		o_ct0, o_ct1,
+	output	reg	[511:0]		o_ct,
 	output	wire			o_done,
-	output	wire			o_key_done
+	output	wire			o_read_pt
 );
 
 	// ***** local parameter definition *****
-	parameter IDLE	= 3'b000;
-	parameter RDY	= 3'b001;
-	parameter BLOCK	= 3'b010;
-	parameter CRYPT	= 3'b110;
-	parameter DONE	= 3'b100;
+//	parameter IDLE	= 2'b00;
+//	parameter BLOCK	= 2'b01;
+//	parameter CRYPT	= 2'b11;
+//	parameter DONE	= 2'b10;
+	parameter IDLE	= 2'd0;
+	parameter BLOCK	= 2'd1;
+	parameter CRYPT	= 2'd2;
+	parameter DONE	= 2'd3;
 
 	// ***** local register definition *****
+	reg		[1:0]	r_fsm;
+	reg		[255:0] r_key;
+	reg		[95:0]	r_non;
+	reg		[31:0]	r_cnt;
+	reg		[511:0]	r_pt;
+	reg		[31:0]	r_len_pt;
 
 	// ***** local wire definition *****
-	wire			w_blk_done, w_blk_done0, w_blk_done1;	// 1: finish BLOCK state
-	wire			w_blk_state;							// 1: have to BLOCK state
-	wire			w_blk_start;							// 1: start BLOCK state
-	wire	[31:0]	w_blk0_cnt, w_blk1_cnt;					// U0_CC_BLOCK: cnt*2, U1_CC_BLOCK: cnt*2+1
-	wire	[511:0]	w_stream0, w_stream1;					// output 'cc_block'
-
-	assign	w_blk_done	= w_blk0_done && w_blk1_done;
-	assign	w_blk_state	= (!r_cnt) ? |r_len_pt[31:6] : |r_len_pt[31:7];	// len of pt that not crypt is big than 64byte & 128byte
-	assign	w_blk_start	= (r_fsm == RDY) || ((r_fsm == CRYPT) && w_blk_state);
-	assign	w_blk0_cnt	= {r_cnt, 1'b0};
-	assign	w_blk1_cnt	= {r_cnt, 1'b1};
-
-	assign	o_done = (r_fsm == DONE);
+	wire			w_blk_state;	// 1: have to BLOCK state
+	wire			w_blk_start;	// 1: start BLOCK state
+	wire			w_blk_done;		// 1: finish BLOCK state
+	wire	[511:0]	w_stream;		// output 'cc_block'
+	
+	assign	w_blk_state	= r_len_pt!=32'd0;
+	assign	w_blk_start	= (i_start) || ((r_fsm == CRYPT) && w_blk_state);
+	
+	assign	o_read_pt	= (w_blk_done && w_blk_state);
+	assign	o_done		= (r_fsm == DONE);
 
 ////////////////////////////////////////////////////////////////////////////////
 
 	// ***** Instantiate the design module *****
 
-	cc_block U0_CC_BLOCK (
-		.i_clk		(	i_clk		),
-		.i_rstn		(	i_rstn		),
-		.i_start	(	w_blk_start	),
-		.i_key		(	r_key		),
-		.i_non		(	r_non		),
-		.i_cnt		(	w_blk0_cnt	),
-
-		.o_stream	(	w_stream0	),
-		.o_done		(	w_blk0_done	)
-	);
-
 	cc_block U1_CC_BLOCK (
 		.i_clk		(	i_clk		),
 		.i_rstn		(	i_rstn		),
 		.i_start	(	w_blk_start	),
-		.i_key		(	r_key		),
-		.i_non		(	r_non		),
-		.i_cnt		(	w_blk1_cnt	),
+		.i_key		(	i_key		),
+		.i_non		(	i_non		),
+		.i_cnt		(	r_cnt		),
 
-		.o_stream	(	w_stream1	),
-		.o_done		(	w_blk1_done	)
+		.o_stream	(	w_stream	),
+		.o_done		(	w_blk_done	)
 	);
 
 ////////////////////////////////////////////////////////////////////////////////
 
 	// Explanation of always statement
 
-	reg	[2:0]	r_fsm;
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_fsm	<= IDLE;
 		else
 			case (r_fsm)
-				IDLE	: r_fsm <= (i_start) ? RDY : IDLE;
-				RDY		: r_fsm <= BLOCK;
+				IDLE	: r_fsm <= (i_start) ? BLOCK : IDLE;
 				BLOCK	: r_fsm <= (w_blk_done) ? CRYPT : BLOCK;
 				CRYPT	: r_fsm <= (w_blk_start) ? BLOCK : DONE;
 				DONE	: r_fsm <= IDLE;
 			endcase
 	end
 
-	reg	[255:0] r_key;
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_key	<= 256'd0;
@@ -90,7 +81,6 @@ module cc_encrypt (
 			r_key	<= (i_start) ? i_key : r_key;
 	end
 
-	reg [95:0] r_non;
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_non	<= 96'd0;
@@ -98,50 +88,41 @@ module cc_encrypt (
 			r_non	<= (i_start) ? i_non : r_non;
 	end
 
-	reg	[511:0] r_pt;
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			r_pt	<= 1024'd0;
-		else
-			r_pt	<= (i_start) ? i_pt : r_pt;
+			r_cnt	<= 32'd1;
+		else if (o_read_pt)
+			r_cnt	<= r_cnt + 1'b1;
+//		else
+//			r_cnt	<= r_cnt;
 	end
 
-	reg	[31:0] r_len_pt;
+	always @(posedge i_clk, negedge i_rstn) begin
+		if (!i_rstn)
+			r_pt	<= 512'd0;
+		else if (i_start || o_read_pt)
+			r_pt	<= i_pt;
+//		else
+//			r_pt	<= r_pt;
+	end
+
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_len_pt	<= 32'd0;
 		else if (i_start)
 			r_len_pt	<= i_len_pt;
-		else if (((r_fsm == RDY) || (r_fsm == CRYPT)) && (w_blk_state))
-			r_len_pt	<= (!r_cnt) ? r_len_pt - 7'd64 : r_len_pt - 8'd128;
-		else
-			r_len_pt	<= r_len_pt;
+		else if (o_read_pt)
+			r_len_pt	<= (r_len_pt<32'd64) ? 32'd0 : r_len_pt - 32'd64;
+//		else
+//			r_len_pt	<= r_len_pt;
 	end
 
-	// o_ct0
+	// encrypt plain text
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			o_ct0	<= 512'd0;
+			o_ct	<= 512'd0;
 		else
-			o_ct0	<= (r_fsm == CRYPT) ? o_ct ^ r_pt : o_ct0;
-	end
-	
-	// o_ct1
-	always @(posedge i_clk, negedge i_rstn) begin
-		if (!i_rstn)
-			o_ct1	<= 512'd0;
-		else
-			o_ct1	<= (r_fsm == CRYPT) ? o_ct1 ^ r_pt : o_ct1;
-	end
-
-	reg [30:0] r_cnt;
-	always @(posedge i_clk, negedge i_rstn) begin
-		if (!i_rstn)
-			r_cnt	<= 31'd0;
-		else if ((r_fsm == BLOCK) && w_blk_done)
-			r_cnt	<= r_cnt + 1'b1;
-		else
-			r_cnt	<= r_cnt;
+			o_ct	<= (w_blk_done) ? w_stream ^ r_pt : o_ct;
 	end
 
 endmodule 
