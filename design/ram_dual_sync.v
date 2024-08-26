2b5cfc7b7ec015f63bf9a0bd889e744a7fa5de53
SeongJun-Ro <seongjunro@gmail.com>
2024-08-26 15:50:13 +0900

    change inout port init

--- design/ram_dual_sync.v
+++ design/ram_dual_sync.v
@@ -1,27 +1,17 @@
-module ram_dual_sync (
-	w_clk, r_clk,
-	w_en,
-	w_data,
-	w_addr,
-	r_en,
-	r_addr,
-	r_data
+module ram_dual_sync #(
+	parameter	D_WIDTH	= 128,
+	parameter	A_WIDTH	= 3
+)(
+	input							w_clk, r_clk,
+	input							w_en,
+	input			[A_WIDTH-1:0]	w_addr,
+	input			[D_WIDTH-1:0]	w_data,
+	input							r_en,
+	input			[A_WIDTH-1:0]	r_addr,
+	output	reg		[D_WIDTH-1:0]	r_data
 );
-	parameter	D_WIDTH = 128;
-	parameter	A_WIDTH = 3;
-	
-	input					w_clk, r_clk;
-	input					w_en;
-	input	[A_WIDTH-1:0]	w_addr;
-	input	[D_WIDTH-1:0]	w_data;
-	input					r_en;
-	input	[A_WIDTH-1:0]	r_addr;
-	output	[D_WIDTH-1:0]	r_data;
-	reg		[D_WIDTH-1:0]	r_data;
-
 
 	reg		[D_WIDTH-1:0] ram [2**A_WIDTH-1:0];
-	
 
 	always @(posedge w_clk) begin
 		if(w_en)
