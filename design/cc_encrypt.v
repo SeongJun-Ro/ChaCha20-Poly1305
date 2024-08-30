2e87d7c500bbd9f2596919ea3ecf045b19e1b92c
SeongJun-Ro <seongjunro@gmail.com>
2024-08-30 14:53:44 +0900

    rm r_cnt=='d0

--- design/cc_encrypt.v
+++ design/cc_encrypt.v
@@ -1,31 +1,26 @@
 module cc_encrypt (
 	input					i_clk, i_rstn,
-	input					i_start,
-	
-	input					i_en_pt,
-	input		[255:0]		i_key,
-	input		[95:0]		i_non,
-	input		[511:0]		i_pt,
-	input		[31:0]		i_len_pt,
+	input					i_start,		// module start
 
-	output	reg	[511:0]		o_ct,
-	output	reg				o_rqst_pt,
-	output	wire			o_done
+	input					i_en_pt,		// enable plaintext
+	input		[255:0]		i_key,			// key
+	input		[95:0]		i_non,			// nonce
+	input		[511:0]		i_pt,			// plaintext
+	input		[31:0]		i_len_pt,		// plaintext length
+
+	output	reg	[511:0]		o_ct,			// ciphertext
+	output	reg				o_rqst_pt,		// request plaintext & using 'o_ct'
+	output	wire			o_done			// module done & using final 'o_ct'
 );
 
-	// ***** local parameter definition *****
-//	parameter IDLE	= 2'b00;
-//	parameter BLOCK	= 2'b01;
-//	parameter CRYPT	= 2'b11;
-//	parameter DONE	= 2'b10;
-	parameter IDLE	= 2'd0;
-	parameter BLOCK	= 2'd1;
-	parameter CRYPT	= 2'd2;
-	parameter DONE	= 2'd3;
-	
+	// fsm parameter
+	parameter IDLE	= 2'b00;
+	parameter BLOCK	= 2'b01;
+	parameter CRYPT	= 2'b11;
+	parameter DONE	= 2'b10;
+	// mask parameter
 	parameter MASK	= 512'hffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
-	// ***** local register definition *****
 	reg		[1:0]	r_fsm;
 	reg		[255:0] r_key;
 	reg		[95:0]	r_non;
@@ -33,22 +28,24 @@ module cc_encrypt (
 	reg		[511:0]	r_pt;
 	reg		[31:0]	r_len_pt;
 
-	// ***** local wire definition *****
 	wire			w_blk_state;	// 1: have to BLOCK state
 	wire			w_blk_start;	// 1: start BLOCK state
 	wire			w_blk_done;		// 1: finish BLOCK state
 	wire	[511:0]	w_stream;		// output 'cc_block'
 	wire	[511:0]	w_mask;
-	
+
+	// If r_len_pt is not 0, it means there are still plaintexts to be processed
+	// 0 if r_len_pt is 0, 1 otherwise
 	assign	w_blk_state	= r_len_pt!=32'd0;
+	// start signal for 'U1_CC_BLOCK'
+	// w_blk_state is not 0, wait i_en_pt high
 	assign	w_blk_start	= (i_start) || ((r_fsm == CRYPT) && i_en_pt);
+	// if the r_pt_len to process is less than 64, mask the data that is not applicable
 	assign	w_mask		= (r_len_pt<32'd64) ? (MASK >> {32'd64-r_len_pt, 3'd0}) : MASK;
-	
+	// 1 when the module is done
 	assign	o_done		= (r_fsm == DONE);
 
 ////////////////////////////////////////////////////////////////////////////////
-
-	// ***** Instantiate the design module *****
 
 	cc_block U1_CC_BLOCK (
 		.i_clk		(	i_clk		),
@@ -57,14 +54,11 @@ module cc_encrypt (
 		.i_key		(	i_key		),
 		.i_non		(	i_non		),
 		.i_cnt		(	r_cnt		),
-
 		.o_stream	(	w_stream	),
 		.o_done		(	w_blk_done	)
 	);
 
 ////////////////////////////////////////////////////////////////////////////////
-
-	// Explanation of always statement
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
@@ -95,13 +89,14 @@ module cc_encrypt (
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			r_cnt	<= 32'd0;
+			r_cnt	<= 32'd1;
 		else if (o_rqst_pt)
 			r_cnt	<= r_cnt + 1'b1;
 //		else
 //			r_cnt	<= r_cnt;
 	end
 
+	// if module start or enable plaintext, load i_pt
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			r_pt	<= 512'd0;
@@ -111,20 +106,20 @@ module cc_encrypt (
 //			r_pt	<= r_pt;
 	end
 
+	// after the BLOCK state is completed, r_len_pt is reduced by 16
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			r_len_pt	<=	32'd0;
+			r_len_pt	<= 32'd0;
 		else if (i_start)
-			r_len_pt	<=	i_len_pt;
+			r_len_pt	<= i_len_pt;
 		else if (w_blk_done)
-			r_len_pt	<=	(!r_cnt==32'd0) ? 
-							(r_len_pt<32'd64) ? 32'd0 : r_len_pt - 32'd64
-							: r_len_pt;
+			r_len_pt	<= (r_len_pt<32'd64) ? 32'd0 : r_len_pt - 32'd64;
 //		else
 //			r_len_pt	<= r_len_pt;
 	end
 
-	// read o_ct and input pt
+	// request plaintext & use o_ct signal
+	// if w_blk_state is 1 when the BLOCK state is completed, 1 otherwise, 0
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			o_rqst_pt	<= 1'd0;
@@ -133,15 +128,13 @@ module cc_encrypt (
 		else
 			o_rqst_pt	<= 1'd0;
 	end
-	
+
 	// encrypt plain text
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			o_ct	<=	512'd0;
+			o_ct	<= 512'd0;
 		else
-			o_ct	<=	(w_blk_done) ? 
-						(r_cnt==32'd0) ? w_stream : w_mask & (w_stream ^ r_pt) 
-						: o_ct;
+			o_ct	<= (w_blk_done) ? w_mask & (w_stream ^ r_pt) : o_ct;
 	end
 
-endmodule 
+endmodule
