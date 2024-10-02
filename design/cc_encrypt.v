e42c1d6ccda446ab4c209c82bcbb9732e65f6d34
SeongJun-Ro <seongjunro@gmail.com>
2024-10-02 17:50:57 +0900

    add o_busy

--- design/cc_encrypt.v
+++ design/cc_encrypt.v
@@ -1,18 +1,20 @@
 module cc_encrypt (
-	input					i_clk, i_rstn,
-	input					i_start,		// module start
+	input						i_clk, i_rstn,
+	input						i_start,		// module start
 
-	input					i_en_pt,		// enable plaintext
-	input		[255:0]		i_key,			// key
-	input		[95:0]		i_non,			// nonce
-	input		[511:0]		i_pt,			// plaintext
-	input		[31:0]		i_len_pt,		// plaintext length
+	input						i_en_pt,		// enable plaintext
+	input			[255:0]		i_key,			// key
+	input			[95:0]		i_non,			// nonce
+	input			[511:0]		i_pt,			// plaintext
+	input			[31:0]		i_len_pt,		// plaintext length
 
-	output	reg	[511:0]		o_ct,			// ciphertext
-	output	reg				o_rqst_pt,		// request plaintext & using 'o_ct'
-	output	wire			o_done			// module done & using final 'o_ct'
+	output	reg		[511:0]		o_ct,			// ciphertext
+	output	reg					o_rqst_pt,		// request plaintext & using 'o_ct'
+	output						o_done,			// module done & using final 'o_ct'
+	output	reg					o_busy			// cc_encrypt module is busy!
 );
 
+	// ***** local parameter definition *****
 	// fsm parameter
 	parameter IDLE	= 2'b00;
 	parameter BLOCK	= 2'b01;
@@ -21,17 +23,17 @@ module cc_encrypt (
 	// mask parameter
 	parameter MASK	= 512'hffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
 
+	// ***** local register definition *****
 	reg		[1:0]	r_fsm;
-	reg		[255:0] r_key;
-	reg		[95:0]	r_non;
 	reg		[31:0]	r_cnt;
 	reg		[511:0]	r_pt;
 	reg		[31:0]	r_len_pt;
 
-	wire			w_blk_state;	// 1: have to BLOCK state
-	wire			w_blk_start;	// 1: start BLOCK state
-	wire			w_blk_done;		// 1: finish BLOCK state
-	wire	[511:0]	w_stream;		// output 'cc_block'
+	// ***** local wire definition *****
+	wire			w_blk_state;
+	wire			w_blk_start;
+	wire			w_blk_done;
+	wire	[511:0]	w_stream;
 	wire	[511:0]	w_mask;
 
 	// If r_len_pt is not 0, it means there are still plaintexts to be processed
@@ -39,13 +41,15 @@ module cc_encrypt (
 	assign	w_blk_state	= r_len_pt!=32'd0;
 	// start signal for 'U1_CC_BLOCK'
 	// w_blk_state is not 0, wait i_en_pt high
-	assign	w_blk_start	= (i_start) || ((r_fsm == CRYPT) && i_en_pt);
+//	assign	w_blk_start	= (i_en_pt && ((r_cnt==32'd1) || (r_fsm == CRYPT)));
+	assign	w_blk_start	= i_start || (i_en_pt && (r_fsm == CRYPT));
 	// if the r_pt_len to process is less than 64, mask the data that is not applicable
 	assign	w_mask		= (r_len_pt<32'd64) ? (MASK >> {32'd64-r_len_pt, 3'd0}) : MASK;
-	// 1 when the module is done
 	assign	o_done		= (r_fsm == DONE);
 
 ////////////////////////////////////////////////////////////////////////////////
+
+	// ***** Instantiate the design module *****
 
 	cc_block U1_CC_BLOCK (
 		.i_clk		(	i_clk		),
@@ -59,6 +63,8 @@ module cc_encrypt (
 	);
 
 ////////////////////////////////////////////////////////////////////////////////
+
+	// Explanation of always statement
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
@@ -75,20 +81,7 @@ module cc_encrypt (
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
-			r_key	<= 256'd0;
-		else
-			r_key	<= (i_start) ? i_key : r_key;
-	end
-
-	always @(posedge i_clk, negedge i_rstn) begin
-		if (!i_rstn)
-			r_non	<= 96'd0;
-		else
-			r_non	<= (i_start) ? i_non : r_non;
-	end
-
-	always @(posedge i_clk, negedge i_rstn) begin
-		if (!i_rstn)
+//			r_cnt	<= 32'd0;
 			r_cnt	<= 32'd1;
 		else if (o_rqst_pt)
 			r_cnt	<= r_cnt + 1'b1;
@@ -123,6 +116,8 @@ module cc_encrypt (
 	always @(posedge i_clk, negedge i_rstn) begin
 		if (!i_rstn)
 			o_rqst_pt	<= 1'd0;
+//		else if (i_start)
+//			o_rqst_pt	<= 1'd1;
 		else if (w_blk_done && w_blk_state)
 			o_rqst_pt	<= (r_len_pt<32'd64) ? 1'b0 : 1'b1;
 		else
@@ -135,6 +130,18 @@ module cc_encrypt (
 			o_ct	<= 512'd0;
 		else
 			o_ct	<= (w_blk_done) ? w_mask & (w_stream ^ r_pt) : o_ct;
+	end
+
+	// cc_encrypt module is busy!
+	always @(posedge i_clk, negedge i_rstn) begin
+		if (!i_rstn)
+			o_busy	<= 1'd0;
+		else if (i_start)
+			o_busy	<= 1'd1;
+		else if (o_done)
+			o_busy	<= 1'd0;
+		else
+			o_busy	<= o_busy;
 	end
 
 endmodule
