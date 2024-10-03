c37f8aa750cb1f7f2164ffe1134be4b06a9cd0e9
SeongJun-Ro <seongjunro@gmail.com>
2024-10-03 14:29:40 +0900

    Update ram_cc2p.v

--- design/ram_cc2p.v
+++ design/ram_cc2p.v
@@ -1,35 +1,42 @@
+// `define TEST
+
 module ram_cc2p (
 	input					i_clk, i_rstn,
 	input					i_en_w, i_en_r,
 	input			[511:0]	i_data,
-	
+
 	output			[127:0]	o_data,
-	output	reg				o_full,
 	output	reg				o_sig,
-	output					o_empty
+//	output	reg				o_full,
+//	output	reg				o_empty
+	output					o_full,
+	output	reg				o_empty
 );
 
 	// ***** local parameter definition *****
 	parameter	D_WIDTH = 128;
-	parameter	A_WIDTH = 16;
+	parameter	A_WIDTH = 3;
 
 	// ***** local register definition *****
-	reg		[A_WIDTH-1:0]	r_addr_w, r_addr_r;
+	reg		[A_WIDTH:0]		r_addr_w, r_addr_r;
 	reg		[511:0]			r_stream;
 	reg		[2:0]			r_cnt_w;
 	reg						r_empty_d1;
-	reg						r_en_r_d1;
+	reg						r_en_r;
 
 	// ***** local wire definition *****
-	wire				w_en_w;
-	wire	[127:0]		w_stream;
-	wire				w_empty;
+	wire					w_en_w;
+	wire	[A_WIDTH-1:0]	w_addr_w, w_addr_r;
+	wire	[127:0]			w_stream;
+	wire	[A_WIDTH:0]		w_chk_full;
 
-	assign	w_en_w		= (r_cnt_w!=3'd0);
-	assign	w_stream	= r_stream[127:0];
-	assign	w_empty		= r_addr_w==r_addr_r;
+	assign	w_en_w		=	(r_cnt_w!=3'd0);
+	assign	w_addr_w	=	r_addr_w[A_WIDTH-1:0];
+	assign	w_addr_r	=	r_addr_r[A_WIDTH-1:0];
+	assign	w_stream	=	r_stream[127:0];
+	assign	w_chk_full	=	r_addr_w + 3'd4;
 	
-	assign	o_empty		= w_empty;
+	assign o_full		=	((w_chk_full[A_WIDTH]!=r_addr_r[A_WIDTH]) && (w_chk_full[A_WIDTH-1:0]>=r_addr_r[A_WIDTH-1:0])) ? 1'd1 : 1'd0;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -41,8 +48,8 @@ module ram_cc2p (
 	) U0_RAMS_DIST (
 		.clk		(	i_clk		),
 		.we			(	w_en_w		),
-		.a			(	r_addr_w	),
-		.dpra		(	r_addr_r	),
+		.a			(	w_addr_w	),
+		.dpra		(	w_addr_r	),
 		.di			(	w_stream	),
 		.spo		(				),
 		.dpo		(	o_data		)
@@ -51,6 +58,36 @@ module ram_cc2p (
 ////////////////////////////////////////////////////////////////////////////////
 
 	// Explanation of always statement
+
+`ifdef TEST
+	always @(posedge i_clk, negedge i_rstn) begin
+		if(!i_rstn)
+			r_addr_w	<=	4'd1;
+		else
+			r_addr_w	<=	(w_en_w || r_cnt_w!=3'd0) ? r_addr_w + 1'd1 : r_addr_w;
+	end
+
+	always @(posedge i_clk, negedge i_rstn) begin
+		if(!i_rstn)
+			r_addr_r	<=	4'd0;
+		else
+			r_addr_r	<=	(r_en_r && !o_empty) ? r_addr_r + 1'd1 : r_addr_r;
+	end
+`else
+	always @(posedge i_clk, negedge i_rstn) begin
+		if(!i_rstn)
+			r_addr_w	<=	17'd1;
+		else
+			r_addr_w	<=	(w_en_w || r_cnt_w!=3'd0) ? r_addr_w + 1'd1 : r_addr_w;
+	end
+
+	always @(posedge i_clk, negedge i_rstn) begin
+		if(!i_rstn)
+			r_addr_r	<=	17'd0;
+		else
+			r_addr_r	<=	(i_en_r) ? r_addr_r + 1'd1 : r_addr_r;
+	end
+`endif
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if(!i_rstn)
@@ -73,50 +110,40 @@ module ram_cc2p (
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if(!i_rstn)
-			r_addr_w	<=	16'd1;
-		else
-			r_addr_w	<=	(w_en_w || r_cnt_w!=3'd0) ? r_addr_w + 1'd1 : r_addr_w;
-	end
-
-	always @(posedge i_clk, negedge i_rstn) begin
-		if(!i_rstn)
-			r_addr_r	<=	16'd0;
-		else
-			r_addr_r	<=	(i_en_r) ? r_addr_r + 1'd1 : r_addr_r;
-	end
-
-	always @(posedge i_clk, negedge i_rstn) begin
-		if(!i_rstn)
 			r_empty_d1	<=	1'd0;
 		else
-			r_empty_d1	<= w_empty;
+			r_empty_d1	<=	o_empty;
 	end
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if(!i_rstn)
-			r_en_r_d1	<=	1'd0;
+			r_en_r	<=	1'd0;
+		else if(i_en_r)
+			r_en_r	<=	1'd1;
+		else if(!o_empty && r_empty_d1)
+			r_en_r	<=	1'd0;
+		else if(!o_empty && r_en_r)
+			r_en_r	<=	1'd0;
 		else
-			r_en_r_d1	<=	i_en_r;
-	end
-
-	always @(posedge i_clk, negedge i_rstn) begin
-		if(!i_rstn)
-			o_full	<=	1'd0;
-		else
-			o_full	<=	((r_addr_w + 3'd4)==r_addr_r) ? 1'd1 : 1'd0;
+			r_en_r	<=	r_en_r;
 	end
 
 	always @(posedge i_clk, negedge i_rstn) begin
 		if(!i_rstn)
 			o_sig	<=	1'd0;
-		else if(w_empty)
-			o_sig	<=	1'd0;
-		else if(r_en_r_d1)
+		else if(r_en_r && !o_empty && r_empty_d1)
 			o_sig	<=	1'd1;
-		else if(!w_empty && r_empty_d1)
+		else if(!o_empty && r_en_r)
 			o_sig	<=	1'd1;
 		else
 			o_sig	<=	1'd0;
+	end
+
+	always @(posedge i_clk, negedge i_rstn) begin
+		if(!i_rstn)
+			o_empty	<=	1'd0;
+		else
+			o_empty	<=	(w_addr_w==w_addr_r+1'b1) ? 1'd1 : 1'd0;
 	end
 
 endmodule
